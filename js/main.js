eb69d99e7da3c63b2af04f63e18239067e0800b9
suparpat <reminiz@gmail.com>
2015-09-10 22:58:29 +0700

    more work on the footer and fixed some formatting

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@
    
            // when done, add hash to url
            // (default click behaviour)
-           //window.location.hash = this.hash;
+           window.location.hash = 'howitworks';
          });
 
     });
