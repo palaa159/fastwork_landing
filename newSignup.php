2be9991635c80c476487efef757328045c705171
Apon Palanuwech <aponpalanuwech@Apons-MacBook-Pro.local>
2015-09-21 16:32:21 +0700

    - fix php

--- newSignup.php
+++ newSignup.php
@@ -1,9 +1,14 @@
 <?php 
-	$value = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : null;
-	if ($value === 'GET') {
-		// Increase number
-		// 
-		// echo "yeah";
+    // …
+	if($_SERVER['REQUEST_METHOD'] === 'POST') {
+		// Get "n" and return
+		$number = file_get_contents("signups.json");
+		$json = json_decode($number, true);
+		$nOfUsers = $json["n"];
+
+		echo $nOfUsers;
+
+	} else {
 		$number = file_get_contents("signups.json");
 		$json = json_decode($number, true);
 		$nOfUsers = $json["n"];
@@ -13,15 +18,5 @@
 		$jsonToSave = Array('n' => $nOfUsers);
 		$jsonToSave = json_encode($jsonToSave);
 		file_put_contents('signups.json', $jsonToSave);
-
-    // …
-	} else if($value === 'POST') {
-		// Get "n" and return
-		$number = file_get_contents("signups.json");
-		$json = json_decode($number, true);
-		$nOfUsers = $json["n"];
-
-		echo $nOfUsers;
-
 	}
 ?>
